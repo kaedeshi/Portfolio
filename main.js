3a10f7a0cffb113eb951b9802c449f483c0a0f72
kaedeshi <149069128+kaedeshi@users.noreply.github.com>
2024-01-17 23:52:43 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,30 @@
 'use strict';
-
 {
-  const about = document.getElementById('about');
-  const work = document.getElementById('work');
-  const pr = document.getElementById('pr');
-  const skill = document.getElementById('skill');
+  const hamburger = document.getElementById('hamburger');
+  const openMenu = document.getElementById('openmenu');
+  const mask = document.getElementById('mask');
+  const menu = document.querySelector('#hamburger span');
+  const menuLists = document.querySelectorAll('#openmenu ul li a');
 
-  const top = document.getElementById('scroll');
- 
-
-  window.addEventListener('scroll',() => {
-    let scrollY = window.scrollY;
-    if(scrollY > 0){
-      about.classList.add('show');
-    }
-    if(scrollY > 0 && window.innerWidth > 600){
-      about.classList.add('show');
-    }
-    if(scrollY > 560){
-      work.classList.add('show');
-    }
-    if(scrollY > 350 && window.innerWidth > 600){
-      work.classList.add('show');
-    }
-    if(scrollY > 1050){
-      pr.classList.add('show');
-    }
-    if(scrollY > 700 && window.innerWidth > 600){
-      pr.classList.add('show');
-    }
-    if(scrollY > 1550){
-      skill.classList.add('show');
-    }
-    if(scrollY > 1050 && window.innerWidth > 600){
-      skill.classList.add('show');
-    }
-
-    if(scrollY > 1000) {
-      top.classList.add('show');
-    }
-    if(scrollY <= 1000) {
-      top.classList.remove('show');
-    }
+  hamburger.addEventListener('click',() => {
+    hamburger.classList.toggle('show');
+    openMenu.classList.toggle('show');
+    mask.classList.toggle('show');
+    menu.classList.toggle('show');
   });
+  mask.addEventListener('click',() => {
+    hamburger.classList.remove('show');
+    openMenu.classList.remove('show');
+    mask.classList.remove('show');
+    menu.classList.remove('show');
+  });
+  menuLists.forEach(menuList => {
+    menuList.addEventListener('click',() =>{
+      hamburger.classList.remove('show');
+      openMenu.classList.remove('show');
+      mask.classList.remove('show');
+      menu.classList.remove('show');
+    });
+  });
+  
 }
